df95d7608a10de339adbc1cc5afc51b073c9e71a
dfch <dofmerxd@gmail.com>
2026-08-05 01:59:54 -0500

    upgrade 2

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,8 @@ clear
 #Alias
 alias start='start-hyprland'
 alias stop='exit'
+alias doom='cage -- ~/Games/doom/chocolate-doom -iwad ~/Games/doom/DOOM1.WAD'
+alias doom-config='cage -- ~/Games/doom/chocolate-setup'
 
 if test "$XDG_CURRENT_DESKTOP" = "Hyprland"
     fastfetch
